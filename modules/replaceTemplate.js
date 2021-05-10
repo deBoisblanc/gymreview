d130a22aa598507460d0be06ba9d6f901edffa5b
deboisblanc <ddebois@gmail.com>
2021-05-10 09:43:15 -0400

    initial commit

---
+++ modules/replaceTemplate.js
@@ -0,0 +1,14 @@
+module.exports = (temp, product) => {
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%MODE%}/g, product.mode);
+  output = output.replace(/{%AMENITIES%}/g, product.amenities);
+  output = output.replace(/{%RATING%}/g, product.rating);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
+
+  if (!product.organic)
+    output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+  return output;
+};
